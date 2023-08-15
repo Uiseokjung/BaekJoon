e11bac72f5efb928ecd8a6b74fbd49d5e5da579a
Uiseokjung <70683738+Uiseokjung@users.noreply.github.com>
2023-08-15 21:55:52 +0900

    8/15

--- b4889.java
+++ b4889.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 public class b4889 {
+	//Nope
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
